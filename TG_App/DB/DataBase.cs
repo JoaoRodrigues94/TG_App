3ba03a434e19d5ac5db796703046d2875c924594
João Paulo Fernandes Rodrigues <joaopaulo.fr.1994@gmail.com>
2021-08-10 23:21:03 -0300

    Criado a string de conexão

---
+++ TG_App/DB/DataBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using TG.Model;
+using Xamarin.Forms;
+
+namespace TG_App.DB
+{
+  class DataBase
+  {
+    // String de Conexão
+    private SQLiteConnection _conexao;
+    public DataBase()
+    {
+      var dep = DependencyService.Get<ICaminho>();
+      string caminho = dep.ObterCaminho("database.sqlite");
+
+      _conexao = new SQLiteConnection(caminho);
+      // Criar tabela
+      _conexao.CreateTable<Usuario>();
+      _conexao.CreateTable<Horarios>();
+    }
+
+    // Métodos  Cadastro
+    public void CadastrarUsuario(Usuario user)
+    {
+      _conexao.Insert(user);
+    }
+    public void CadastrarHorario(Horarios horario)
+    {
+      _conexao.Insert(horario);
+    }
+
+    // Métodos Update
+    public void UpdateUsuario(Usuario user)
+    {
+      _conexao.Update(user);
+    }
+    public void UpdateHorario(Horarios horarios)
+    {
+      _conexao.Update(horarios);
+    }
+    // Métodos Exclusão
+    public void DeleteUsuario(Usuario user)
+    {
+      _conexao.Delete(user);
+    }
+    public void DeleteUsuario(Horarios horario)
+    {
+      _conexao.Delete(horario);
+    }
+  }
+}
---
+++ TG_App/DB/ICaminho.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TG_App.DB
+{
+  public interface ICaminho
+  {
+    string ObterCaminho(string NomeArquivoBanco);
+  }
+}
