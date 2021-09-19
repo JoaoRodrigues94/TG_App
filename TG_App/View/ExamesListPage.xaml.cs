c1df401e679fdb44af3f8a5ea3d8b8c2fe936c6e
João Paulo Fernandes Rodrigues <joaopaulo.fr.1994@gmail.com>
2021-09-18 23:29:44 -0300

    Filtro de Exames

--- TG_App/View/ExamesListPage.xaml.cs
+++ TG_App/View/ExamesListPage.xaml.cs
@@ -60,5 +60,62 @@ namespace TG_App.View
     {
       App.Current.MainPage = new RegistrarExamePage();
     }
+
+    public void Pesquisar(object sender, EventArgs args)
+    {
+      DBExame DB = new DBExame();
+      DBSugestao DB2 = new DBSugestao();
+      var user = new Validacao().Listagem().SingleOrDefault();
+
+      var listaE = DB.Pesquisar().ToList();
+      var listaS = DB2.Pesquisar().ToList();
+
+      if (!String.IsNullOrEmpty(DataSearch.Text))
+      {
+        listaE = listaE.Where(x => x.Data.Contains(DataSearch.Text)).ToList();
+        listaS = listaS.Where(x => x.Data.Contains(DataSearch.Text)).ToList();
+      }
+
+      if (!String.IsNullOrEmpty(ResultadoSearch.Text))
+      {
+        listaE = listaE.Where(x => x.Resultado == ResultadoSearch.Text).ToList();
+        listaS = listaS.Where(x => x.Resultado == ResultadoSearch.Text).ToList();
+      }
+
+      if (!String.IsNullOrEmpty(SugestaoSearch.Text))
+      {
+        listaE = new List<Exame>();
+        listaS = listaS.Where(x => x.Dosagem == Convert.ToInt32(SugestaoSearch.Text)).ToList();
+      }
+
+      List<SugestaoView> dados = new List<SugestaoView>();
+
+      foreach (var item in listaE)
+      {
+        SugestaoView x = new SugestaoView
+        {
+          Data = "DATA: " + item.Data,
+          Resultado = "RESULTADO DO EXAME: " + item.Resultado,
+          TipoSugestao = "E"
+        };
+        dados.Add(x);
+      }
+      foreach (var item in listaS)
+      {
+        SugestaoView x = new SugestaoView
+        {
+          Data = item.Data + ":00",
+          Resultado = "RESULTADO DO EXAME: " + item.Resultado,
+          Dosagem = "SUGESTÃO DE DOSAGEM: " + item.Dosagem.ToString() + " UNIDADES",
+          TipoSugestao = "S"
+        };
+        dados.Add(x);
+      }
+
+      ListaExame.ItemsSource = dados.OrderBy(x => x.Data);
+      DataSearch.Text = null;
+      ResultadoSearch.Text = null;
+      SugestaoSearch.Text = null;
+    }
   }
 }
